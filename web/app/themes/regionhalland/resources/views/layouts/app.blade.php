067cc369aef9ffefd4b4ed28375c89f9c148b434
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-04 20:43:53 +0200

    En del av region halland banner added

--- web/app/themes/regionhalland/resources/views/layouts/app.blade.php
+++ web/app/themes/regionhalland/resources/views/layouts/app.blade.php
@@ -3,9 +3,9 @@
     @include('partials.head')
     <body style="height: 101%" @php(body_class())>
         @include('partials.jump-to-content')
+        @include('partials.part-of-region-halland')
         @include('partials.cookie-notice')
         @include('partials.header')
-
 
         @yield('content')
         @include('partials.footer')
---
+++ web/app/themes/regionhalland/resources/views/partials/part-of-region-halland.blade.php
@@ -0,0 +1,1 @@
+<div class="py1" style="display:flex; justify-content:center; background: #FCF800;">En del av Region Halland</div>
