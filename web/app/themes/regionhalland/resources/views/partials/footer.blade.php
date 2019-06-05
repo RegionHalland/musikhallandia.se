fb7a0e0ded294c245086be8eefeb1614cea245bf
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-05 15:15:50 +0200

    Content and colour of footer adjusted

--- web/app/themes/regionhalland/resources/views/partials/footer.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/footer.blade.php
@@ -1,87 +1,85 @@
-<footer class="rh-footer-logo mx-auto mt1" id="footer-top-placeholder" style="max-width: 1440px;">
+<footer id="footer-top-placeholder" style="background-color: #EFE7DA; line-height: 1.4;">
+    <div class="clearfix center px3 pb3" style="max-width: 1440px;">
+        <div class="left-align col col-12 sm-col-12 md-col-4 lg-col-4">
+            <img style="" src="{!! env('WP_HOME') !!}/include/img/MH_Greyscale.png" />
+            <p style="max-width:15em;">
+                Musik Hallandia är en musikinstitution, helägd av Region Halland.
+            </p>
+            <p>
+                <a href="">Facebook</a>
+            </p>
+            <p>
+                <a href="">Instagram</a>
+            </p>
+            <p>
+                <a href="">E-post</a>
+            </p>
 
-    <div class="rh-footer-logo-div">
-        <a href="https://www.regionhalland.se"><img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide5.0.0/images/components/kommuner_logo/RegionHalland.png" alt="RegionHalland Logo"></a>
-    </div>
-    <div class="rh-footer-logo-div">
-        <a href="https://kommun.falkenberg.se/"><img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide5.0.0/images/components/kommuner_logo/Falkenberg.png" alt="Falkenberg Logo"></a>
-    </div>
-    <div class="rh-footer-logo-div">
-        <a href="https://www.halmstad.se/"><img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide5.0.0/images/components/kommuner_logo/Halmstad.png" alt="Halmstad Logo"></a>
-    </div>
-    <div class="rh-footer-logo-div">
-        <a href="http://hylte.se/"><img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide5.0.0/images/components/kommuner_logo/Hylte.png" alt="Hylte Logo"></a>
-    </div>
-    <div class="rh-footer-logo-div">
-        <a href="https://www.laholm.se/"><img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide5.0.0/images/components/kommuner_logo/Laholm.png" alt="Laholm Logo"></a>
-    </div>
-    <div class="rh-footer-logo-div">
-        <a href="https://varberg.se/"><img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide5.0.0/images/components/kommuner_logo/Varberg.png" alt="Varberg Logo"></a>
-    </div>
+        </div>
 
+        <div class="left-align col col-12 sm-col-12 md-col-2 lg-col-2">
+            <p class="h2 pt3">Sidor</p>
+            @php($first_level_pages = get_region_halland_tree_first_level())
+            @if(isset($first_level_pages) && !empty($first_level_pages))
+                <ul>
+                    @foreach($first_level_pages as $first_level_page)
+                        @if($first_level_page->active === true)
+                            <li>
+                                <a class="active" href="{{ $first_level_page->url }}">{{ $first_level_page->post_title }}</a>
+                            </li>
+                        @else
+                            <li>
+                                <a href="{{ $first_level_page->url }}">{{ $first_level_page->post_title }}</a>
+                            </li>
+                        @endif
+                    @endforeach
+                </ul>
+            @endif
+        </div>
+        <div class="left-align col col-12 sm-col-12 md-col-3 lg-col-3">
+            <p class="h2 pt3">Om oss</p>
+            <ul>
+                <li><a href="/om-oss">Om oss</a></li>
+                <li><a href="/">Vår vision</a></li>
+                <li><a href="/">Vårt uppdrag</a></li>
+                <li><a href="/">Lediga jobb</a></li>
+                <li><a href="/">Kontakt</a></li>
+                <li><a href="/">Press & Media</a></li>
+            </ul>
+        </div>
+
+        <div class="left-align col col-12 sm-col-12 md-col-3 lg-col-3">
+            <p class="h2 pt3">
+                Kontakt
+            </p>
+            <p>
+                <strong>Besöksadress:</strong><br>
+                Kultur i Halland<br>
+                Kronobränneriet<br>
+                Skonertgatan 12A, Halmstad
+            </p>
+            <p>
+                <strong>Postadress:</strong><br>
+                Region Halland<br>
+                Kultur i Halland<br>
+                Box 517, 301 80 Halmstad<br>
+            </p>
+            <p>
+                <strong>Telefon:</strong> 035 - 13 48 00<br>
+                <strong>Fax:</strong> 035 - 13 54 44
+            </p>
+            <p>
+                <a href="">Kontakta personalen</a>
+            </p>
+
+        </div>
+    </div>
 </footer>
 
+
+
 <script src="{!! env('WP_HOME') !!}/include/scripts/jquery.3.3.1.min.js?ver=3.3.1"></script>
-
-<script src="{!! env('WP_HOME') !!}/styleguide5.0.0/js/components.js"></script>
-
 <script>
-    function debounce(delay, fn) {
-        var timer = null;
-        return function () {
-            var context = this, args = arguments;
-            clearTimeout(timer);
-            timer = setTimeout(function () {
-                fn.apply(context, args);
-            }, delay);
-        };
-    }
-
-    $('a[href^="#"]').on( "click", function() {
-        var target = $(this.hash);
-        if (target.length) {
-
-            // Animate target
-            $('html,body').animate({scrollTop: target.offset().top}, 800);
-
-            // Add class for highlight the text
-            $(target).addClass("content-highlight");
-
-            // Wait 2s and then remove the highlight class
-            setTimeout(function(){
-                $(target).removeClass("content-highlight");
-            }, 2000);
-
-            return false;
-        }
-    });
-
-    if ($("body.page-template-default")[0]){
-        $(window).scroll(debounce(250, function() {
-
-            var myPosition = Math.round($('#content-nav-placeholder').offset().top - $(window).scrollTop());
-            var myFooterTop = Math.round($('#footer-top-placeholder').offset().top - $(window).scrollTop());
-            var myContentNavBottom = Math.round($('#content-nav-bottom-placeholder').offset().top - $(window).scrollTop());
-            if (myPosition < 30) {
-                if (myFooterTop < myContentNavBottom + 40) {
-                    $(".content-nav__item").addClass("content-nav__item--tight");
-                } else {
-                    $("#content-nav-container").addClass("rh-get-fixed-sticky");
-                    $(".content-nav__item--tight").removeClass("content-nav__item--tight");
-                }
-            } else {
-                $("#content-nav-container").removeClass("rh-get-fixed-sticky");
-                $(".content-nav__item--tight").removeClass("content-nav__item--tight");
-
-            }
-        }));
-    }
-
-    $(".rh-header-button--search").on( "click", function() {
-        $(".rh-header-search-desktop").show();
-        $(".rh-header-button--search").hide();
-    });
-
     // **************************************
     // *** Javascript set cookie function ***
     // **************************************
@@ -105,17 +103,6 @@
         // Hide div with cookie notice text + button
         $(".rh-cookie").hide();
     });
-
 </script>
-<script type="text/javascript">var _baTheme=0, _baMode='Aktivera Talande Webb', _baUseCookies=true, _baHideOnLoad=true;</script>
 
-<!-- How it looked before -->
-<!-- <script type="text/javascript" src="//www.browsealoud.com/plus/scripts/ba.js"></script> -->
-<script type="text/javascript" src="{!! env('WP_HOME') !!}/include/aloud/ba.js"></script>
 
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-1181886-12');
-</script>
