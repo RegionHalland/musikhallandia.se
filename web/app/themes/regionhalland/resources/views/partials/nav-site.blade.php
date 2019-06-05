bcf463df209318991c7003bc06f5309cfb8a7da6
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-05 13:05:24 +0200

    Full width background colour

--- web/app/themes/regionhalland/resources/views/partials/nav-site.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/nav-site.blade.php
@@ -1,11 +1,13 @@
-<nav aria-label="Sidhuvud" class="mx-auto" style="max-width: 1440px; background: #EFE7DA;">
-    <img style="" src="{!! env('WP_HOME') !!}/include/img/MH_Greyscale.png" />
-    <ul class="" style="display: inline; padding-left:auto;">
-        @php($first_level_pages = get_region_halland_tree_first_level())
-        @foreach($first_level_pages as $index => $page)
-            <li class="mx1" style="display:inline;">
-                <a class="" href="{{ $page->url }}">{{ $page->post_title }}</a>
-            </li>
-        @endforeach
-    </ul>
-</nav>
+<div style="background: #EFE7DA;">
+    <nav aria-label="Sidhuvud" class="mx-auto" style="max-width: 1440px; background: #EFE7DA;">
+        <img style="" src="{!! env('WP_HOME') !!}/include/img/MH_Greyscale.png" />
+        <ul class="" style="display: inline; padding-left:auto;">
+            @php($first_level_pages = get_region_halland_tree_first_level())
+            @foreach($first_level_pages as $index => $page)
+                <li class="mx1" style="display:inline;">
+                    <a class="" href="{{ $page->url }}">{{ $page->post_title }}</a>
+                </li>
+            @endforeach
+        </ul>
+    </nav>
+</div>
