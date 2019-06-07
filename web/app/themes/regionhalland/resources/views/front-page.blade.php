60a8e2809552df908b4a8d2cdc53eb3f59765684
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-07 10:22:47 +0200

    Slight cleanup and commenting

--- web/app/themes/regionhalland/resources/views/front-page.blade.php
+++ web/app/themes/regionhalland/resources/views/front-page.blade.php
@@ -1,15 +1,21 @@
 @extends('layouts.app')
 @section('content')
     @include('partials.hero')
+    {{-- Ingress-container --}}
     <div style="background:#EFE7DA;">
         <div class="center p3" style="max-width:50em; line-height:1.4;">
             {{ get_region_halland_acf_page_ingress() }}
             <p><a href="">Läs mer om oss</a><span class="ml1 icon-arrow-right" style="font-family: feather !important;"></span></p>
         </div>
     </div>
-    <div>
-        Test
+
+    {{-- Huvudinnehåll --}}
+    <div class="center px3" style="max-width: 1440px;">
+        <main>
+            Här hamnar själva sidinnehållet
+        </main>
     </div>
+    
 
 
 @endsection
