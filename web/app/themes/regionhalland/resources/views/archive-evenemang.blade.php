254950556647c3bb22cc3585eb09ba4aeb2adc9e
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-10 14:39:35 +0200

    The archive page created

---
+++ web/app/themes/regionhalland/resources/views/archive-evenemang.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+@section('content')
+    @include('partials.hero')
+    {{-- Ingress-container --}}
+    <div style="background:#EFE7DA;">
+        <div class="center p3" style="max-width:50em; line-height:1.4;">
+            Musik Hallandia möjliggör ett brett utbud av musikupplevelser - allt från festivaler och konserter till musikprogram och aktiviteter för barn och unga. Vi har något för alla, helt enkelt. Vill du veta mer? Hör av dig!
+            <p><a href="">Läs mer om oss</a><span class="ml1 icon-arrow-right" style="font-family: feather !important;"></span></p>
+        </div>
+    </div>
+
+    {{-- Huvudinnehåll --}}
+    <div class="center px3 rh-article" style="max-width: 1440px;">
+        <main>
+            <div style="display:flex; align-content: center;"><h1>Aktuella evenemang</h1></div>
+            @php($myData = get_region_halland_acf_page_evenemang_kommande_items())
+            @if(isset($myData))
+                <div class="clearfix">
+
+                    @foreach ($myData as $data)
+                        <div class="col col-12 md-col-4 px1">
+                            <div style="background: #777777;">{!! $data->image !!}<br></div>
+                            <strong><a href="{{ $data->url }}">{{ $data->post_title }}</a></strong><br>
+                            {{ $data->ingress }}
+                        </div>
+                    @endforeach
+                </div>
+            @endif
+        </main>
+    </div>
+
+    @include('partials.newsletter')
+
+@endsection
+
+
