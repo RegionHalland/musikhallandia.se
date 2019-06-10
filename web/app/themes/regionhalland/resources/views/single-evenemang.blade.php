e031f9c9a78e1fc940b56e84a34c573e4de73ca5
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-10 14:16:01 +0200

    Right hand box tweaked

--- web/app/themes/regionhalland/resources/views/single-evenemang.blade.php
+++ web/app/themes/regionhalland/resources/views/single-evenemang.blade.php
@@ -26,88 +26,43 @@
                             {{ the_content() }}
                         @endwhile
                     </div>
-                    <div class="p2 col col-12 md-col-3" style="background: #EFE7DA;">
-                        <div class="pb3 mt2" style="border-bottom: 1px solid black;">
-                            <h2>Information</h2><br>
-                            <p>
-                                <strong>Stad:</strong> {{ get_region_halland_acf_page_evenemang_stad() }}
-                            </p>
-                            <p>
-                                <strong>Spelställe:</strong> {{ get_region_halland_acf_page_evenemang_spelstalle() }}
-                            </p>
-                            <p>
-                                <strong>Speltid:</strong> {{ get_region_halland_acf_page_evenemang_speltid() }}
-                            </p>
-                            <p>
-                                <strong>Länkar:</strong>
+                    <div class="pl3 col col-12 md-col-3" >
+                        <div class="p2 " style="background: #EFE7DA;">
+                            <div class="pb3 mt2" style="border-bottom: 1px solid black;">
+                                <h2>Information</h2><br>
+                                <p>
+                                    <strong>Var:</strong> {{ get_region_halland_acf_page_evenemang_spelstalle() }}, {{ get_region_halland_acf_page_evenemang_stad() }}
+                                </p>
+                                <p>
+                                    <strong>När:</strong> {{ get_region_halland_acf_page_evenemang_speltid() }}
+                                </p>
+                                <p>
+                                    <strong>Länkar:</strong>
 
-                            @php($myInformation = get_region_halland_acf_page_evenemang_information())
-                            @foreach ($myInformation as $information)
-                                @if($information['has_link'])
-                                    <a href="{{ $information['link_url'] }}" target="{{ $information['link_target'] }}">{{ $information['link_title'] }}</a><br>
-                                @endif
-                            @endforeach
-                            </p>
-                        </div>
-                        <div class="pb3 mt3 mb3" style="border-bottom: 1px solid black;">
-                            <h2>Arrangörer</h2>
-                            <p>
-                            @php($myArrangor = get_region_halland_acf_page_evenemang_arrangor())
-                            @foreach ($myArrangor as $arrangor)
-                                @if($arrangor['has_link'])
-                                    <a href="{{ $arrangor['link_url'] }}" target="{{ $arrangor['link_target'] }}">{{ $arrangor['link_title'] }}</a><br>
-                                @endif
-                            @endforeach
-                            </p>
+                                @php($myInformation = get_region_halland_acf_page_evenemang_information())
+                                @foreach ($myInformation as $information)
+                                    @if($information['has_link'])
+                                        <a href="{{ $information['link_url'] }}" target="{{ $information['link_target'] }}">{{ $information['link_title'] }}</a><br>
+                                    @endif
+                                @endforeach
+                                </p>
+                            </div>
+                            <div class="pb3 mt3 mb3" style="border-bottom: 1px solid black;">
+                                <h2>Arrangörer</h2>
+                                <p>
+                                @php($myArrangor = get_region_halland_acf_page_evenemang_arrangor())
+                                @foreach ($myArrangor as $arrangor)
+                                    @if($arrangor['has_link'])
+                                        <a href="{{ $arrangor['link_url'] }}" target="{{ $arrangor['link_target'] }}">{{ $arrangor['link_title'] }}</a><br>
+                                    @endif
+                                @endforeach
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </main>
         </div>
     </div>
-
-    {{--
-    <!-- ************ -->
-    <!-- Page content -->
-    <!-- ************ -->
-    <div class="background-white">
-        <div class="background-white">
-            <div class="container mx-auto p4">
-                <div class="m2 flex flex-wrap">
-
-                    <div class="col-12 lg-col-3">
-
-                        @include('partials.nav-sidebars')
-
-                        <div class="pt2">&nbsp;</div>
-
-                        @include('partials.content-nav')
-
-                    </div>
-
-                    <!-- ************ -->
-                    <!-- Page content -->
-                    <!-- ************ -->
-                    <div class="col-12 lg-col-9">
-                        <main class="ml4">
-
-                            <div>
-                                <h1>{{ the_title() }}</h1>
-                            </div>
-
-                            <div id="main">
-                                @while(have_posts()) @php(the_post())
-                                    @include('partials.article')
-                                    @include('partials.entry-meta')
-                                @endwhile
-                            </div>
-
-                        </main>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </div> --}}
 
 @endsection
