762ecd818349a839784d0c2d5f02918d48233557
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-17 08:57:35 +0200

    Commenting out partial I think is unused

--- web/app/themes/regionhalland/resources/views/partials/article.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/article.blade.php
@@ -1,6 +1,8 @@
+{{--
 @if(function_exists('get_region_halland_prepare_the_content'))
 	@php(get_region_halland_prepare_the_content())
 @endif
 <article @php(post_class('article'))>
 	@php(the_content())
 </article>
+--}}
