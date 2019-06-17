4d7dfd5e4bb436f86cf00749428f57030149a4d4
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-17 07:42:30 +0200

    Partial for second level navigation bar added

---
+++ web/app/themes/regionhalland/resources/views/partials/nav-level2.blade.php
@@ -0,0 +1,16 @@
+@if(function_exists('get_region_halland_page_children'))
+    @php($myPages = get_region_halland_page_children())
+    @if(isset($myPages))
+        <div class="py2" style="background: #EFE7DA;">
+            <nav aria-label="Navigering nivå två" class="flex items-center justify-between mx-auto" style="max-width: 1440px; background: #EFE7DA;">
+                <ul class="" style="display: inline;">
+                    @foreach ($myPages as $myChilds)
+                        <li class="mx1" style="display:inline;">
+                            <a href="{{ $myChilds->url }}">{{ $myChilds->post_title }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </nav>
+        </div>
+    @endif
+@endif
