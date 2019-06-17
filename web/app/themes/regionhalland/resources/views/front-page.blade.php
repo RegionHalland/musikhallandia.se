92c224ee727bddd8e1fe661de48a328a3d2d4c4f
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-17 11:15:51 +0200

    Headline centered

--- web/app/themes/regionhalland/resources/views/front-page.blade.php
+++ web/app/themes/regionhalland/resources/views/front-page.blade.php
@@ -12,7 +12,7 @@
     {{-- Huvudinnehåll --}}
     <div class="center px3 rh-article" style="max-width: 1440px;">
         <main>
-            <div style="display:flex; align-content: center;"><h1>Aktuella evenemang</h1></div>
+            <div style="width:100%; text-align: center;"><h1>Aktuella evenemang</h1></div>
             @php($myData = get_region_halland_acf_page_evenemang_kommande_items())
             @if(isset($myData))
                 <div class="clearfix">
