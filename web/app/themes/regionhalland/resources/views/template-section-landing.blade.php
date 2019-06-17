01974bab9d933f1e0ba396ef74cd204b20fef311
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-17 07:41:54 +0200

    Fixing template section landing

--- web/app/themes/regionhalland/resources/views/template-section-landing.blade.php
+++ web/app/themes/regionhalland/resources/views/template-section-landing.blade.php
@@ -9,19 +9,18 @@
 @section('content')
 
     @include('partials.hero')
-
-    <div class="rh-xpad-A pt3 pb3" style="background: #FBF9F4">
-        <div class="rh-xpad-B mx-auto" style="max-width: 1440px;">
-            <main>
+    @include('partials.nav-level2')
+    <main>
+        <div class="rh-xpad-A pt3 pb3" style="background: #FBF9F4">
+            <div class="rh-xpad-B mx-auto" style="max-width: 1440px;">
                 <div class="clearfix">
-
                     {{-- Content --}}
                     <div class="col col-12 md-col-9 rh-article">
                         @while(have_posts()) @php(the_post())
                         <h1>{{ $post->post_title }}</h1>
 
                         <strong>
-                            {{ get_region_halland_acf_page_evenemang_ingress() }}
+                            {{ get_region_halland_acf_page_ingress() }}
                         </strong>
 
                         <p>
@@ -30,17 +29,30 @@
                         @endwhile
                     </div>
                 </div>
-
-                {{-- Blurbs på nedre delen av sidan --}}
-                <div style="background: #EFE7DA;">
-                    TODO: HÄR SKALL BLURBAR LIGGA OM DET FINNS NÅGRA PÅ SIDAN
-                </div>
-            </main>
-
-
-
+            </div>
         </div>
 
-    </div>
+        {{-- Blurbs på nedre delen av sidan --}}
+        <div class="rh-xpad-A pt3 pb3" style="background: #EFE7DA;">
+            <div class="rh-xpad-B mx-auto" style="max-width: 1440px;">
+                @php($myBlurbs = get_region_halland_acf_main_post_page_links_blurbs())
+                @if(isset($myBlurbs))
+                    <ul>
+                    @foreach ($myBlurbs as $blurbs)
+                        <li class="pb4">
+                            <div class="clearfix">
+                                <div class="mr3" style="float:left;">
+                                    {!! $blurbs['image'] !!}
+                                </div>
+                                <h2>{{ $blurbs['post_title'] }}</h2>
+                                {{ $blurbs['post_content'] }}
+                            </div>
+                        </li>
+                    @endforeach
+                    </ul>
+                @endif
+            </div>
+        </div>
+    </main>
     @include('partials.newsletter')
 @endsection
