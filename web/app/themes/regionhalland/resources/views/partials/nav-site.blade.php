251fa2c4c89afca324b9f09a0b70bea7fbc707d1
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-17 08:27:52 +0200

    More failsafe way to link home logo

--- web/app/themes/regionhalland/resources/views/partials/nav-site.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/nav-site.blade.php
@@ -1,6 +1,6 @@
 <div style="background: #EFE7DA;">
     <nav aria-label="Sidhuvud" class="flex items-center justify-between mx-auto" style="max-width: 1440px; background: #EFE7DA;">
-        <a href="/"><img style="" src="{!! env('WP_HOME') !!}/include/img/MH_Greyscale.png" /></a>
+        <a href="{!! env('WP_HOME') !!}"><img style="" src="{!! env('WP_HOME') !!}/include/img/MH_Greyscale.png" /></a>
         <ul class="" style="display: inline;">
             @php($first_level_pages = get_region_halland_tree_first_level())
             @foreach($first_level_pages as $index => $page)
