44c0661da5c66c17485123dda93aace4c1de1fb7
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-17 07:43:23 +0200

    Navigation to sibling partial added

---
+++ web/app/themes/regionhalland/resources/views/partials/nav-siblings.blade.php
@@ -0,0 +1,19 @@
+<div class="py2" style="background: #EFE7DA;">
+    <nav aria-label="Navigering nivå två" class="flex items-center justify-between mx-auto" style="max-width: 1440px; background: #EFE7DA;">
+        <ul class="" style="display: inline;">
+
+                <li class="mx1" style="display:inline;">
+                    <a href="/">Navigationsval 1</a>
+                </li>
+                <li class="mx1" style="display:inline;">
+                    <a href="/">Navigationsval 2</a>
+                </li>
+                <li class="mx1" style="display:inline;">
+                    <a href="/">Navigationsval 3</a>
+                </li>
+
+
+
+        </ul>
+    </nav>
+</div>
