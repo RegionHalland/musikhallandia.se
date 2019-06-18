9d60c1c3dce4375c58ed53d0d7fc5c0a6bb219c2
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-18 17:54:52 +0200

    Start of implementation for hero widget with labels and such

--- web/app/themes/regionhalland/resources/views/front-page.blade.php
+++ web/app/themes/regionhalland/resources/views/front-page.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    @include('partials.hero')
+    @include('partials.hero-widget')
     {{-- Ingress-container --}}
     <div style="background:#EFE7DA;">
         <div class="center p3" style="max-width:40em; text-align:center; line-height:1.4;">
--- web/app/themes/regionhalland/resources/views/partials/hero-widget.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/hero-widget.blade.php
@@ -1,7 +1,8 @@
+@php($myData = get_region_halland_acf_page_evenemang_kommande_items())
 @if(get_the_post_thumbnail_url())
-    <div style="min-height: 400px; max-height: 500px; background-image: url('{{ get_the_post_thumbnail_url() }}');  height: 50%; background-position: center; background-repeat: no-repeat; background-size: cover; position: relative;">
+    <div style="min-height: 400px; max-height: 500px; background-image: url('{!! $myData[0]->image_url !!}');  height: 50%; background-position: center; background-repeat: no-repeat; background-size: cover; position: relative;">
         <div class="center rh-caption-hero" style="max-width: 1440px;">
-            {{ $post->post_title }}
+            {{ $myData[0] -> post_title }}
         </div>
     </div>
 @endif
