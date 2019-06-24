d99ae0affd1f06742876c80d3718d19968e38eaa
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-24 15:38:51 +0200

    Only showing headline and content divs when there is actual events

--- web/app/themes/regionhalland/resources/views/front-page.blade.php
+++ web/app/themes/regionhalland/resources/views/front-page.blade.php
@@ -12,9 +12,10 @@
     {{-- Huvudinnehåll --}}
     <div class="center px3 rh-article" style="max-width: 1440px;">
         <main>
-            <div style="width:100%; text-align: center;"><h1>Aktuella evenemang</h1></div>
+
             @php($myData = get_region_halland_acf_page_evenemang_kommande_items())
-            @if(isset($myData))
+            @if(isset($myData[0]))
+                <div style="width:100%; text-align: center;"><h1>Aktuella evenemang</h1></div>
                 <div class="clearfix">
 
                 @foreach ($myData as $data)
