08b13c9ddc3a3f3b9add9786578bdf5172a54f3c
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-27 13:02:09 +0200

    Some work on the contact cards

--- web/app/themes/regionhalland/resources/views/template-contact.blade.php
+++ web/app/themes/regionhalland/resources/views/template-contact.blade.php
@@ -35,23 +35,25 @@
                     {{-- Kontaktkorten från nya pluginet --}}
                     @php($myData = get_region_halland_acf_main_post_page_contact_cards())
                     @if(isset($myData))
-                        <div class="col col-12">
-                            <ul>
+                        <div>
+                            <ul style="display:inline-block">
                             @foreach ($myData as $data)
-                                <li class="col col-12 sm-col-12 md-col-6 lg-col-4 pb4">
-                                    <div class="clearfix">
-                                        @if($data['contact_has_image'])
-                                            <div class="mr3" style="text-align:center; overflow: hidden; position: relative;">
-                                                <img src="{{ $data['contact_image_url'] }}">
-                                                {{-- Decorative triangles --}}
-                                                <span style="width:0; height: 0; position: absolute; bottom: 0; left: 0; border-left: 30px solid #FA3CB4; border-top: 80px solid transparent;"></span>
-                                                <span style="width:0; height: 0; position: absolute; top: 0; right: 0; border-right: 30px solid #FA3CB4; border-bottom: 80px solid transparent;"></span>
-                                            </div>
-                                        @endif
-                                        <h2>{!! $data['contact_name'] !!}</h2>
-                                            {!! $data['post_content'] !!}
-                                        <p>{{ $data['contact_epost'] }}</p>
-                                    </div>
+                                <li class="pb4" style="display:inline-block">
+                                    @if($data['contact_has_image'])
+                                        <div class="mr3" style="text-align:center; overflow: hidden; position: relative; width:400px; height:300px; ">
+                                            <img src="{{ $data['contact_image_url'] }}" style="width:400px; height:300px;">
+                                            {{-- Decorative triangles --}}
+                                            <span style="width:0; height: 0; position: absolute; bottom: 0; left: 0; border-left: 30px solid #FA3CB4; border-top: 80px solid transparent;"></span>
+                                            <span style="width:0; height: 0; position: absolute; top: 0; right: 0; border-right: 30px solid #FA3CB4; border-bottom: 80px solid transparent;"></span>
+                                        </div>
+                                    @endif
+                                    <h2>
+                                        {!! $data['contact_name'] !!}
+                                    </h2>
+                                    {!! $data['post_content'] !!}
+                                    <p>
+                                        {{ $data['contact_epost'] }}
+                                    </p>
                                 </li>
                             @endforeach
                             </ul>
