9e604eb343d0db2f1b9c3152c942ab27070d8fd8
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-27 14:55:45 +0200

    Deleting old unused code

--- web/app/themes/regionhalland/resources/views/partials/nav-level2.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/nav-level2.blade.php
@@ -1,21 +1,3 @@
-{{-- @if(function_exists('get_region_halland_page_children'))
-    @php($myPages = get_region_halland_page_children())
-    @if(isset($myPages))
-        <div class="py2" style="background: #EFE7DA;">
-            <nav aria-label="Navigering nivå två" class="flex items-center justify-between mx-auto" style="max-width: 1440px; background: #EFE7DA;">
-                <ul class="" style="display: inline;">
-                    @foreach ($myPages as $myChilds)
-                        <li class="mx1" style="display:inline;">
-                            <a class="rh-link--navigation" href="{{ $myChilds->url }}">{{ $myChilds->post_title }}</a>
-                        </li>
-                    @endforeach
-                </ul>
-            </nav>
-        </div>
-    @endif
-@endif --}}
-
-
 @if(function_exists('get_region_halland_tree_first_second_level_child_pages'))
     @php($myPages = get_region_halland_tree_first_second_level_child_pages())
     @if(isset($myPages) && !empty($myPages['first_page']))
