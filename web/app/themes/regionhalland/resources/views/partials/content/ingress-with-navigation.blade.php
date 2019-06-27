5de9e102fca10ad3ca6b6932f1423d11ea7c2e1d
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-27 12:59:47 +0200

    New partials as start of organizing the partials in groups of related smaller units

---
+++ web/app/themes/regionhalland/resources/views/partials/content/ingress-with-navigation.blade.php
@@ -0,0 +1,12 @@
+{{-- Ingress-container --}}
+<div style="background:#EFE7DA;">
+    <div class="center p3" style="max-width:40em; text-align:center; line-height:1.4;">
+        {{ $ingress_text }}
+        <p>
+            <a class="rh-link--navigation" href="{{ $link_url }}">
+                <strong>{{ $linktext  }}</strong>
+            </a>
+            <span class="ml1 icon-arrow-right" style="font-family: feather !important;"></span>
+        </p>
+    </div>
+</div>
