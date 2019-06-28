af3acb335dbd9af255e639c04d8afbd2f522ab1d
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-28 13:17:24 +0200

    Removing favicon code for region halland favicons

--- web/app/themes/regionhalland/resources/views/partials/sitewide/html-head.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/sitewide/html-head.blade.php
@@ -2,13 +2,7 @@
 	<meta charset="utf-8" />
 	<meta http-equiv="x-ua-compatible" content="ie=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
-	<link rel="apple-touch-icon" sizes="180x180" href="{!! env('WP_HOME') !!}/include/favicons/apple-touch-icon.png" />
-	<link rel="icon" type="image/png" sizes="32x32" href="{!! env('WP_HOME') !!}/include/favicons/favicon-32x32.png" />
-	<link rel="icon" type="image/png" sizes="16x16" href="{!! env('WP_HOME') !!}/include/favicons/favicon-16x16.png"/>
-	<link rel="manifest" href="{!! env('WP_HOME') !!}/include/favicons/site.webmanifest" />
-	<link rel="mask-icon" href="{!! env('WP_HOME') !!}/include/favicons/safari-pinned-tab.svg" color="#6ba3cb" />
-	<link rel="shortcut icon" href="{!! env('WP_HOME') !!}/include/favicons/favicon.ico" />
+	
 	<meta name="apple-mobile-web-app-title" content="Musik Hallandia" />
 	<meta name="application-name" content="Musik Hallandia" />
 	<meta name="msapplication-TileColor" content="#ffffff" />
