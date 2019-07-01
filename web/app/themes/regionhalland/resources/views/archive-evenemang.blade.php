09bcab79c9e1b994147573c42435ad2cd06ade9d
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-01 16:06:08 +0200

    Removing link

--- web/app/themes/regionhalland/resources/views/archive-evenemang.blade.php
+++ web/app/themes/regionhalland/resources/views/archive-evenemang.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     @include('partials.content.hero-widget')
-    @include('partials.content.ingress-with-navigation', array('link_url' => 'https://www.musikhallandia.se', 'linktext' =>'Läs mer om oss', 'ingress_text' => 'Musik Hallandia är en musikinstitution helägd av Region Halland med målet att vara hela Hallands musikplattform. Vi vill förstärka kulturens kraft för att främja kreativiteten och öppenheten i regionen - och för att bygga ett musikliv för alla.'))
+    @include('partials.content.ingress-with-navigation', array('ingress_text' => 'Musik Hallandia är en musikinstitution helägd av Region Halland med målet att vara hela Hallands musikplattform. Vi vill förstärka kulturens kraft för att främja kreativiteten och öppenheten i regionen - och för att bygga ett musikliv för alla.'))
     @include('partials.content.event-grid')
     @include('partials.content.newsletter')
 
