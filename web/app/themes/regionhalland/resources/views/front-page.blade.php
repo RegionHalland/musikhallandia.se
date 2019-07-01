73bff765318d12e2d593e0e37f4b642698e67e67
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-01 15:34:00 +0200

    Mirroring content between musikhallandia.se and musikhallandia.se/evenemang

--- web/app/themes/regionhalland/resources/views/front-page.blade.php
+++ web/app/themes/regionhalland/resources/views/front-page.blade.php
@@ -2,32 +2,7 @@
 @section('content')
     @include('partials.content.hero-widget')
     @include('partials.content.ingress-with-navigation', array('link_url' => 'https://www.musikhallandia.se', 'linktext' =>'Läs mer om oss', 'ingress_text' => 'Musik Hallandia är en musikinstitution helägd av Region Halland med målet att vara hela Hallands musikplattform. Vi vill förstärka kulturens kraft för att främja kreativiteten och öppenheten i regionen - och för att bygga ett musikliv för alla.'))
-
-    {{-- Huvudinnehåll --}}
-    <div class="center px3 rh-article" style="max-width: 1440px;">
-        <main>
-            <div style="width:100%; text-align: center;">
-                <h1>Aktuella evenemang</h1>
-            </div>
-            @php($myData = get_region_halland_acf_page_evenemang_kommande_items())
-            @if(isset($myData[0]))
-
-                <div class="clearfix">
-
-                    @foreach ($myData as $data)
-                        <div class="col col-12 md-col-4 px1">
-                            <div style="background: #777777;">{!! $data->image !!}<br></div>
-                            <strong><a class="rh-link--navigation" href="{{ $data->url }}">{{ $data->post_title }}</a></strong><br>
-                            {{ $data->ingress }}
-                        </div>
-                    @endforeach
-                </div>
-            @else
-                <div class="pb2" style="text-align: center;">Just nu har vi inga aktuella evenemang.</div>
-            @endif
-        </main>
-    </div>
-
+    @include('partials.content.event-grid')
     @include('partials.content.newsletter')
 
 @endsection
---
+++ web/app/themes/regionhalland/resources/views/partials/content/event-grid.blade.php
@@ -0,0 +1,21 @@
+<div class="center px3 rh-article" style="max-width: 1440px;">
+    <main>
+        <div style="width:100%; text-align: center;"><h1>Aktuella evenemang</h1></div>
+        @php($myData = get_region_halland_acf_page_evenemang_kommande_items())
+        @if(isset($myData[0]))
+
+            <div class="clearfix">
+
+                @foreach ($myData as $data)
+                    <div class="col col-12 md-col-4 px1">
+                        <div style="background: #777777;">{!! $data->image !!}<br></div>
+                        <strong><a class="rh-link--navigation" href="{{ $data->url }}">{{ $data->post_title }}</a></strong><br>
+                        {{ $data->ingress }}
+                    </div>
+                @endforeach
+            </div>
+        @else
+            <div class="pb2" style="text-align: center;">Just nu har vi inga aktuella evenemang.</div>
+        @endif
+    </main>
+</div>
