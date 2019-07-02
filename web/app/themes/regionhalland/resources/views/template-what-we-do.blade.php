91ac3b5fc9b6fe50a00047563e398def6ac09854
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-02 15:23:37 +0200

    Tweaking breakpoints to prevent scaling images

--- web/app/themes/regionhalland/resources/views/template-what-we-do.blade.php
+++ web/app/themes/regionhalland/resources/views/template-what-we-do.blade.php
@@ -18,10 +18,18 @@
                         @php($myPages = get_region_halland_page_children())
                         @if(isset($myPages))
                             @foreach ($myPages as $myChilds)
-                                <div class="col col-12 sm-col-6 md-col-4" style="height:400px; overflow: hidden; display: flex; flex-wrap: wrap; justify-content: center;">
+                                <div class="col col-12 sm-col-12 md-col-6 lg-col-4" style="height:400px; overflow: hidden; display: flex; flex-wrap: wrap; justify-content: center;">
                                     <div class="p1" style="display: block;">
                                         <img src="{{ $myChilds->image_url }}" style="width:400px; height: 250px;" />
-                                        <p style="display:block;"><a href="{{ $myChilds->url }}">{{ $myChilds->post_title }}</a></p>
+
+                                        <p style="display:block;">
+                                            <strong>
+                                                <a class="rh-link--navigation" href="{{ $myChilds->url }}">
+                                                    {{ $myChilds->post_title }}
+                                                </a>
+                                            </strong>
+                                        </p>
+
                                     </div>
 
                                 </div>
