a839a9f3d400c2bb899afd902e98e4ad3413c4e3
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-02 14:24:09 +0200

    Max width + grey box on sides for hero

--- web/app/themes/regionhalland/resources/views/partials/content/hero.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/content/hero.blade.php
@@ -1,4 +1,6 @@
 @if(get_the_post_thumbnail_url())
-    <div class="mx-auto" style="background: rgb(211, 211, 210); max-width:1440px; min-height: 400px; max-height: 420px; background-image: url('{{ get_the_post_thumbnail_url() }}');  height: 50%; background-position: center; background-repeat: no-repeat; background-size: cover; position: relative;">
+    <div style="background: rgb(211, 211, 210);">
+        <div class="mx-auto" style="max-width:1440px; min-height: 400px; max-height: 420px; background-image: url('{{ get_the_post_thumbnail_url() }}');  height: 50%; background-position: center; background-repeat: no-repeat; background-size: cover; position: relative;">
+        </div>
     </div>
 @endif
