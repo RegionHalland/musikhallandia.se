d2746e2ffc5fb15dd3e03162025864d99d42d19d
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-02 14:23:12 +0200

    Blurbs got max width behaviour

--- web/app/themes/regionhalland/resources/views/page.blade.php
+++ web/app/themes/regionhalland/resources/views/page.blade.php
@@ -11,7 +11,7 @@
 	@include('partials.content.hero')
 	@include('partials.navigation.nav-level2')
 	<main>
-		<div class="rh-xpad-A pt3 pb3" style="background: #FBF9F4">
+		<div class="rh-xpad-A pt3 pb3 mx-auto" style="max-width: 1440px; background: #FBF9F4">
 			<div class="rh-xpad-B mx-auto" style="max-width: 1440px;">
 				<div class="clearfix">
 					{{-- Content --}}
@@ -35,8 +35,8 @@
 		{{-- Blurbs på nedre delen av sidan --}}
 		@php($myBlurbs = get_region_halland_acf_main_post_page_links_blurbs())
 		@if($myBlurbs)
-			<div class="rh-xpad-A pt3 pb3" style="background: #EFE7DA;">
-				<div class="rh-xpad-B mx-auto" style="max-width: 1440px;">
+			<div class="rh-xpad-A pt3 pb3 mx-auto" style="max-width:1440px; background: #EFE7DA;">
+				<div class="rh-xpad-B">
 					<ul>
 						@foreach ($myBlurbs as $blurbs)
 							<li class="pb4">
