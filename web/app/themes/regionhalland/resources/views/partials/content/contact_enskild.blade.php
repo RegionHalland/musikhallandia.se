2635cb03d5a54f59da18adfef2c30eafbbf003e5
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-03 13:26:06 +0200

    Contact title field used

--- web/app/themes/regionhalland/resources/views/partials/content/contact_enskild.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/content/contact_enskild.blade.php
@@ -9,20 +9,14 @@
 		</strong>
 	</div>
 
-	{{$myContact->post_title}}<br>
-	{!! wpautop($myContact->post_content) !!}
-
-	Namn: {{get_region_halland_acf_page_contact_card_name($cid)}}<br>
-	Epost: {{get_region_halland_acf_page_contact_card_epost($cid)}}<br>
-				
-	@php($myLink = get_region_halland_acf_page_contact_card_link($cid))
-	@if($myLink['has-link'] == 1)
-		<a href="{{$myLink['link-url']}}" target="{{$myLink['link-target']}}">{{$myLink['link-title']}}</a><br>
-	@endif
-
 	@php($myImage = get_region_halland_acf_page_contact_card_image($cid))
 	@if($myImage['has-image'] == 1)
 		<img src="{{$myImage['image-url']}}" width="{{$myImage['image-width']}}" height="{{$myImage['image-height']}}">
 	@endif
+
+	<h1>{{get_region_halland_acf_page_contact_card_name($cid)}}</h1>
+	<p>{{get_region_halland_acf_page_contact_card_title($cid)}}</p>
+	<p><strong>E-post:</strong> {{get_region_halland_acf_page_contact_card_epost($cid)}}</p>
+	{!! wpautop($myContact->post_content) !!}
 
 </div>
