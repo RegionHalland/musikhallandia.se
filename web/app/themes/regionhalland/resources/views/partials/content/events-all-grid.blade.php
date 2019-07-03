6a9e7d7fd2aa81198c40de1da8ec0011c2b534a0
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-03 15:15:09 +0200

    Pill buttons for current/all implemented

--- web/app/themes/regionhalland/resources/views/partials/content/events-all-grid.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/content/events-all-grid.blade.php
@@ -3,6 +3,13 @@
 
 <div class="center px3 rh-article" style="background: #FBF9F4; max-width: 1440px;">
     <main id="main">
+        <div class="mx-auto py3" style="text-align: center">
+            <a href="./" class="rh-pillbutton rh-pillbutton--grey mr2" style="height: 3em; padding-left:1em; padding-right: 1em; border-radius: 3em;">Aktuella evenemang</a>
+            <span class="rh-pillbutton rh-pillbutton--pink" style="height: 3em; padding-left:1em; padding-right: 1em; border-radius: 3em;">Alla evenemang</span>
+        </div>
+
+        
+
         <div style="width:100%; text-align: center;"><h1>Alla evenemang</h1></div>
         @php($myData = get_region_halland_acf_page_evenemang_kommande_items())
         @if(isset($myData[0]))
--- web/app/themes/regionhalland/resources/views/partials/content/events-current-grid.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/content/events-current-grid.blade.php
@@ -1,6 +1,10 @@
 
 <div class="center px3 rh-article" style="background: #FBF9F4; max-width: 1440px;">
     <main id="main">
+        <div class="mx-auto py3" style="text-align: center">
+            <span class="rh-pillbutton rh-pillbutton--pink mr2" style="height: 3em; padding-left:1em; padding-right: 1em; border-radius: 3em;">Aktuella evenemang</span>
+            <a href="./?all=1" class="rh-pillbutton rh-pillbutton--grey" style="height: 3em; padding-left:1em; padding-right: 1em; border-radius: 3em;">Alla evenemang</a>
+        </div>
         <div style="width:100%; text-align: center;"><h1>Aktuella evenemang</h1></div>
         @php($myData = get_region_halland_acf_page_evenemang_kommande_items(2))
         @if(isset($myData[0]))
