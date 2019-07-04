646661e63aa3f19227a9089713afffe2c74cb348
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-04 13:37:07 +0200

    Redoing buttons on events

--- web/app/themes/regionhalland/resources/views/partials/content/events-current-grid.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/content/events-current-grid.blade.php
@@ -2,8 +2,8 @@
 <div class="center px3 rh-article" style="background: #FBF9F4; max-width: 1440px;">
     <main id="main">
         <div class="mx-auto py3" style="text-align: center">
-            <span class="rh-pillbutton rh-pillbutton--pink mr2" style="height: 3em; padding-left:1em; padding-right: 1em; border-radius: 3em;">Aktuella evenemang</span>
-            <a href="./?all=1" class="rh-pillbutton rh-pillbutton--grey" style="height: 3em; padding-left:1em; padding-right: 1em; border-radius: 3em;">Alla evenemang</a>
+            <span class="rh-pillbutton rh-pillbutton--grey mr2" style="height: 3em; padding-left:1em; padding-right: 1em; border-radius: 3em;">Aktuella evenemang</span>
+            <a href="./?all=1" class="rh-pillbutton rh-pillbutton--pink" style="height: 3em; padding-left:1em; padding-right: 1em; border-radius: 3em;">Alla evenemang</a>
         </div>
         <div style="width:100%; text-align: center;"><h1>Aktuella evenemang</h1></div>
         @php($myData = get_region_halland_acf_page_evenemang_kommande_items(2))
