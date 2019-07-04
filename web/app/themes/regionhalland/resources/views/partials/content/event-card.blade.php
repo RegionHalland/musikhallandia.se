09d390f3a2afa517ae1b95a2838eba94433cf20c
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-04 16:50:19 +0200

    Date on event cards

--- web/app/themes/regionhalland/resources/views/partials/content/event-card.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/content/event-card.blade.php
@@ -2,9 +2,13 @@
 
     @foreach ($myData as $data)
         <div class="col col-12 md-col-4 px1">
-            <div>{!! $data->image !!}</div>
+            <div style="position:relative;height:300px; width: 400px; overflow:hidden;">
+                {!! $data->image !!}
+                <span style="padding:1em; border-top-right-radius:10px; border-bottom-right-radius: 10px; color: white; background:black; position:absolute; bottom:1em; left:0; ">{{ $data->speltid_datum }}</span>
+            </div>
             <strong><a class="rh-link--navigation" href="{{ $data->url }}">{{ $data->post_title }}</a></strong><br>
             {{ $data->ingress }}
+
         </div>
     @endforeach
 </div>
