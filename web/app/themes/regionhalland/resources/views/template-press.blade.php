edd217e9b009e4ae1e6137fa47b77249a9577570
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-04 14:37:43 +0200

    Starting to add styling to news function

--- web/app/themes/regionhalland/resources/views/template-press.blade.php
+++ web/app/themes/regionhalland/resources/views/template-press.blade.php
@@ -11,14 +11,8 @@
     @include('partials.content.hero')
     @include('partials.navigation.nav-level2')
     <main id="main">
-        <div class="rh-xpad-A pt3 pb3 mx-auto" style="max-width: 1440px; background: #FBF9F4">
-            <div class="rh-xpad-B mx-auto" style="max-width: 1440px;">
-                <div class="clearfix">
-                    @include('partials.content.content')
-                    @include('partials.content.simple-news')
-                </div>
-            </div>
-        </div>
+        @include('partials.content.content')
+        @include('partials.content.simple-news')
     </main>
     @include('partials.content.newsletter')
 @endsection
