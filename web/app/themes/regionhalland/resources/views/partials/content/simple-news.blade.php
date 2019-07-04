69e0f7d282d50f9dbcbbe9e8a38a1eb90cfd3636
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-04 14:38:06 +0200

    News blade

--- web/app/themes/regionhalland/resources/views/partials/content/simple-news.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/content/simple-news.blade.php
@@ -1,15 +1,23 @@
-<div class="rh-xpad-A pt3 pb3 mx-auto" style="max-width: 1440px; background: #FBF9F4">
+<div class="rh-xpad-A pb3 pt1 mx-auto" style="max-width: 1440px; background: #EFE7DA;">
     <div class="rh-xpad-B mx-auto" style="max-width: 1440px;">
         <div class="clearfix">
-            @php($myNews = get_region_halland_get_page_news_simple())
-			@if($myNews)
-			  @foreach ($myNews as $nyhet)
-			    <h3>{{ $nyhet->post_title }}</h3>
-			    <p>{{ $nyhet->description }}</p>
-			    <p>{!! wpautop($nyhet->post_content) !!}</p>
-			    <a href="{{ $nyhet->link_url }}" target="{{ $nyhet->link_target }}">{{ $nyhet->link_title }}</a><br><br>
-			  @endforeach
-			@endif
+            <h2>Nyheter</h2>
         </div>
     </div>
 </div>
+
+@php($myNews = get_region_halland_get_page_news_simple())
+@if($myNews)
+    <div class="rh-xpad-A pb3 pt1 mx-auto" style="max-width: 1440px; background: #EFE7DA;">
+        <div class="rh-xpad-B mx-auto" style="max-width: 1440px;">
+            <div class="clearfix">
+                @foreach ($myNews as $nyhet)
+                    <h3>{{ $nyhet->post_title }}</h3>
+                    <p>{{ $nyhet->description }}</p>
+                    <p>{!! wpautop($nyhet->post_content) !!}</p>
+                    <a href="{{ $nyhet->link_url }}" target="{{ $nyhet->link_target }}">{{ $nyhet->link_title }}</a><br><br>
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endif
