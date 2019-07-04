34a245eccac8573630237ef4952a5dfa372c13e2
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-04 18:01:14 +0200

    Images locked to correct size + linking headline to post for news

--- web/app/themes/regionhalland/resources/views/partials/content/simple-news.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/content/simple-news.blade.php
@@ -10,14 +10,19 @@
 @if($myNews)
     <div class="rh-xpad-A pb3 pt1 mx-auto" style="max-width: 1440px; background: #EFE7DA;">
         <div class="rh-xpad-B mx-auto" style="max-width: 1440px;">
-            <div class="clearfix">
                 @foreach ($myNews as $nyhet)
-                    <h3>{{ $nyhet->post_title }}</h3>
+                <div class="clearfix">
+
+                    @if($nyhet->image_url)
+                        <img src="{{ $nyhet->image_url }}" style="width:300px;height:200px; float:left;">
+                    @endif
+
+                    <h3><a href="{{ $nyhet->url }}">{{ $nyhet->post_title }}</a></h3>
                     <p>{{ $nyhet->description }}</p>
                     <p>{!! wpautop($nyhet->post_content) !!}</p>
                     <a href="{{ $nyhet->link_url }}" target="{{ $nyhet->link_target }}">{{ $nyhet->link_title }}</a><br><br>
+                </div>
                 @endforeach
-            </div>
         </div>
     </div>
 @endif
