576b457d0dc108f7a9bc6fdb3c5d92dd793a6f30
rhn014 <Roland.Hyden@RegionHalland.se>
2019-07-05 10:43:21 +0200

    Justerat tillbaka-länk på nyhete

--- web/app/themes/regionhalland/resources/views/single-nyhet.blade.php
+++ web/app/themes/regionhalland/resources/views/single-nyhet.blade.php
@@ -6,7 +6,7 @@
         <div class="rh-xpad-A pt3 pb1 mx-auto" style="max-width: 1440px; background: #FBF9F4">
             <div class="rh-xpad-B mx-auto" style="max-width: 1440px;">
                 <div class="">
-                    <a href="om-oss/press-media/" class="rh-round-button rh-round-button--small icon-arrow-left"></a>
+                    <a href="for-press/" class="rh-round-button rh-round-button--small icon-arrow-left"></a>
                     <strong>
                         <a href="/om-oss/press-media/" class="h3 rh-link--navigation" style="line-height: 1.4;">
                             Press & Media
