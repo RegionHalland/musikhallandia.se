9d9fec9996c10a808b4126e8ee9ec4de6a76b53c
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-26 14:23:42 +0200

    Lagt till en h2 'nyheter' som korrigerar rubrikstrukturen

--- web/app/themes/regionhalland/resources/views/partials/content/simple-news-all.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/content/simple-news-all.blade.php
@@ -2,6 +2,11 @@
         
 @php($myNews = get_region_halland_get_page_news_simple())
 @if($myNews)
+    <div class="rh-xpad-A pt2 mx-auto" style="max-width: 1440px;background: #EFE7DA;">
+        <div class="rh-xpad-B">
+            <h2>Nyheter</h2>
+        </div>
+    </div>
     <ul>
         @foreach ($myNews as $nyhet)
             <li class="rh-xpad-A py3 mx-auto rh-hallandia-newslist--newsitem" style="max-width: 1440px;">
