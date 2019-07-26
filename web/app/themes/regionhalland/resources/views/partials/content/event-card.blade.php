9e6c6e38f24322ce25bc70905778bc20ae4ffda9
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-26 16:21:28 +0200

    Plockade bort en fast bredd som clashade med flödet i gridsystemet

--- web/app/themes/regionhalland/resources/views/partials/content/event-card.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/content/event-card.blade.php
@@ -1,9 +1,9 @@
 @php($myData = get_region_halland_acf_page_evenemang_kommande_items())
 @if(isset($myData))
-    <div class="clearfix">
-        <div class="pl4 pt3" style="width:1440px;">
+    <div class="pl4 pt3">
+        <div class="clearfix" style="width:1440px;">
         @foreach ($myData as $data)
-            <div class="col col-12 md-col-4 px1" style="width: 400px;">
+            <div class="col col-12 md-col-4 px1">
                 <div class="pl2 pr2" style="position:relative;height:300px; width: 400px; overflow:hidden">
                     <img src="{{$data->puff_url}}" width="{{$data->puff_width}}" height="{{$data->puff_height}}">
                     <span style="padding:1em; border-top-right-radius:10px; border-bottom-right-radius: 10px; color: white; background:black; position:absolute; bottom:2em; left:1.5em; ">{{ $data->speltid_datum }}</span>
