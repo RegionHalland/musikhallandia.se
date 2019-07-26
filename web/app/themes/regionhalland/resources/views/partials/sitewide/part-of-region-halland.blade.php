24efd18f9c85f372a31b6bdd780a38176f69ab00
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-26 15:02:32 +0200

    Semantisk uppmärkning

--- web/app/themes/regionhalland/resources/views/partials/sitewide/part-of-region-halland.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/sitewide/part-of-region-halland.blade.php
@@ -1,1 +1,5 @@
-<div class="py1 mx-auto" style="text-align:center; max-width:1440px; background: #FCF800;">En del av Region Halland</div>
+<header>
+    <div class="py1 mx-auto" style="text-align:center; max-width:1440px; background: #FCF800;">
+        En del av Region Halland
+    </div>
+</header>
