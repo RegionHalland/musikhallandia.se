721036b1ad7789cc4e786641a1f551632680f3cc
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-26 14:04:12 +0200

    Fixat ul som inte hade li som barn

--- web/app/themes/regionhalland/resources/views/partials/content/simple-news-all.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/content/simple-news-all.blade.php
@@ -4,26 +4,23 @@
 @if($myNews)
     <ul>
         @foreach ($myNews as $nyhet)
-            <div class="rh-xpad-A py3 mx-auto rh-hallandia-newslist--newsitem" style="max-width: 1440px;">
+            <li class="rh-xpad-A py3 mx-auto rh-hallandia-newslist--newsitem" style="max-width: 1440px;">
                 <div class="rh-xpad-B">
-                    <li class="">
-                        <div class="clearfix">
+                    <div class="clearfix">
 
-                            @if($nyhet->image_url)
-                                <img class="mr2" src="{{$nyhet->image_url}}" style="width:300px;height:200px; float:left;">
-                            @endif
+                        @if($nyhet->image_url)
+                            <img class="mr2" src="{{$nyhet->image_url}}" style="width:300px;height:200px; float:left;">
+                        @endif
 
-                            <h3 class="h2"><a class="rh-link--navigation" href="./?nid={{$nyhet->ID}}">{{$nyhet->post_title}}</a></h3>
-                            <p>{{str_limit($nyhet->post_date, $limit = 11, $end = '')}}</p>
-                            
-                            <p>{{$nyhet->description}}</p>
-                            
-                            <a href="{{$nyhet->link_url}}" target="{{$nyhet->link_target}}">{{$nyhet->link_title}}</a><br><br>
+                        <h3 class="h2"><a class="rh-link--navigation" href="./?nid={{$nyhet->ID}}">{{$nyhet->post_title}}</a></h3>
+                        <p>{{str_limit($nyhet->post_date, $limit = 11, $end = '')}}</p>
 
-                        </div>
-                    </li>
+                        <p>{{$nyhet->description}}</p>
+
+                        <a href="{{$nyhet->link_url}}" target="{{$nyhet->link_target}}">{{$nyhet->link_title}}</a>
+                    </div>
                 </div>
-            </div>
+            </li>
         @endforeach
     </ul>
 @endif
