07d562574cb9cba4fafa46c83babec873b76832a
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-29 14:49:51 +0200

    ALT-text på puffar från 2.8.0 av event pluginet

--- web/app/themes/regionhalland/resources/views/partials/content/event-card.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/content/event-card.blade.php
@@ -5,7 +5,7 @@
         @foreach ($myData as $data)
             <div class="col col-12 md-col-4 px1">
                 <div class="pl2 pr2" style="position:relative;height:300px; width: 400px; overflow:hidden">
-                    <img src="{{$data->puff_url}}" width="{{$data->puff_width}}" height="{{$data->puff_height}}">
+                    <img src="{{$data->puff_url}}" width="{{$data->puff_width}}"  alt="{{ $data->puff_alt }}" height="{{$data->puff_height}}">
                     <span style="padding:1em; border-top-right-radius:10px; border-bottom-right-radius: 10px; color: white; background:black; position:absolute; bottom:2em; left:1.5em; ">{{ $data->speltid_datum }}</span>
                 </div>
                 <div class="pl2 pb3">
