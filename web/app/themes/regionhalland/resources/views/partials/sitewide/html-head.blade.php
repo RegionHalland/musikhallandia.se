9184e567dee8ffbdbf32c874959a8471afdc0169
rhn014 <Roland.Hyden@RegionHalland.se>
2019-08-30 15:12:52 +0200

    Lagt till version 1.1.0-5.1.0 av styleguide

--- web/app/themes/regionhalland/resources/views/partials/sitewide/html-head.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/sitewide/html-head.blade.php
@@ -34,8 +34,11 @@
 		<title>{{ $myPostType }} - Musik Hallandia</title>
 	@endif
     <link rel="stylesheet" type="text/css" href="{!! env('WP_HOME') !!}/include/alicdn/font_o5hd5vvqpoqiwwmi_190529.css">
-	<link href="{!! env('WP_HOME') !!}/styleguide5.0.0/css/components.css" rel="stylesheet"/>
-	{{--<link href="http://styleguide.local/musikhallandia.css" rel="stylesheet"/>--}}
+	<link href="{!! env('WP_HOME') !!}/styleguide_cdn/musikhallandia.1.1.0-5.1.0.css" rel="stylesheet"/>
+	{{--
+		<link href="{!! env('WP_HOME') !!}/styleguide5.0.0/css/components.css" rel="stylesheet"/>
+		<link href="http://styleguide.local/musikhallandia.css" rel="stylesheet"/>
+	--}}
 	<link rel="stylesheet" href="{!! env('WP_HOME') !!}/include/style/development.css" type="text/css" media="all" />
 	<script src="{!! env('WP_HOME') !!}/include/scripts/jquery.3.3.1.min.js?ver=3.3.1"></script>
 	<script src="{!! env('WP_HOME') !!}/include/scripts/bodyScrollLock-2.6.4.js"></script>
